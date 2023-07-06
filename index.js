4aba090aff517004cf9988bee8fdda094c82663a
MishaHudz <misha.hudz@gmail.com>
2023-07-06 15:49:38 +0300

    project init, write add, delete contact functions,

---
+++ index.js
@@ -0,0 +1,34 @@
+const argv = require("yargs").argv;
+
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require("./contacts");
+
+// TODO: рефакторити;
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      // ...
+      break;
+
+    case "get":
+      // ... id
+      break;
+
+    case "add":
+      // ... name email phone
+      break;
+
+    case "remove":
+      // ... id
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+// invokeAction(argv);
